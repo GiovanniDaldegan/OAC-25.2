546c7f7609aeb608e7b0cf734ddb5bdf321ee78e
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-10-31 18:20:55 -0300

    mux4 32b

---
+++ LAB2/dev/mux4.v
@@ -0,0 +1,29 @@
+/* Multiplexador de 4 entradas de 32b cada*/
+
+module mux4 (
+   input  wire [31:0] entr0, entr1, entr2, entr3,
+   input  wire [1:0]  select,
+   output wire [31:0] out
+   );
+
+initial
+begin
+   select <= 2'd0;
+   out    <= 32'd0;
+end
+
+always @(*)
+begin
+   case (select)
+      2b'00:
+         out <= entr0;
+      2b'01:
+         out <= entr1;
+      2b'10:
+         out <= entr2;
+      2b'11:
+         out <= entr3;
+   endcase
+end
+
+endmodule
