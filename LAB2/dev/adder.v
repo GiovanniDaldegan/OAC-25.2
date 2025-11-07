8e6b71112b01d27fd56ab1b4ea072dd577602822
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-11-07 19:44:56 -0300

    adder 32b

---
+++ LAB2/dev/adder.v
@@ -0,0 +1,14 @@
+/* Somador de 32 bits */
+
+module adder (
+   input  wire [31:0] iA;
+   input  wire [31:0] iB;
+   output wire [31:0] out;
+);
+
+always @(*)
+begin
+   out <= iA + iB;
+end
+
+endmodule
