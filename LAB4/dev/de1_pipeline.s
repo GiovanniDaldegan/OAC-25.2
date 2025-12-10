857cbd53567097334ebd12807e68c787294e1df6
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-12-10 15:42:44 -0300

    fix: de1_pipeline.s
    nova correção do programa de teste. todas as instruções com hazards que usam WB (escrevem no BancoReg) precisam de 3 nops
    
    como as instruções de tipo-R de teste com t0 não têm hazard entre si, não precisamos de nops entre elas, permitindo maior eficiência
    
    OBS: instruções que usam WB só terão resultado visível somente depois de 4 ciclos após seu início (estágios de IF até WB)

--- LAB4/dev/de1_pipeline.s
+++ LAB4/dev/de1_pipeline.s
@@ -9,33 +9,47 @@
         nop
         nop
         nop
-MAIN:	lw      t1, 0(gp)
+MAIN:	lw      t1, 0(gp)		# gp: 0x1001_0000
         addi    t2, zero, 0x777
         nop
         nop
-        nop
-        and     t0, t1, t2
+        nop				# t1: 0xFFFFFF0F
+        and     t0, t1, t2		# t2: 0x00000777
         or      t0, t1, t2
         add     t0, t2, t1
         sub     t0, t2, t1
-        slt     t0, t1, t2
-        slt     t0, t2, t1
-        beq     t0, zero, PULA
-        nop
+        slt     t0, t1, t2		# t0: 0x00000707 t1 and t2
+        slt     t0, t2, t1		# t0: 0xFFFFFF7F
+        nop				# t0: 0x00000686
+        nop				# t0: 0xFFFFF798
+        nop				# t0: 0x00000001
+        beq     t0, zero, PULA		# t0: 0x00000000
+        nop				# -> PULA
         addi    t0, zero, 0xFFFFFEEE
+        nop
+        nop
+        nop
         j       FIM
         nop
 PULA:	jal     PROC
-        nop
+        nop				# -> PROC
         addi    t0, zero, 0xFFFFFCCC
-FIM:	j       FIM
         nop
+        nop
+        nop
+FIM:	j       FIM			# t0: 0xFFFFFCCC
+        nop				# -> FIM
         
-PROC:	li      t0, 127
+PROC:	addi    t0, t0, 127
         nop
-        sw      t0, 4(gp)
-        lw      t0, 0(gp)
+        nop
+        nop
+        sw      t0, 4(gp)		# t0: 0x0000007F
+        lw      t0, 0(gp)		# sw 0x0000007F 4(gp)
         lw      t0, 4(gp)
-        ret
+	nop
+	nop
+	nop				# t0: 0xFFFFFF0F
+        ret				# t0: 0x0000007F
         nop
-        nop
+        nop				# total de 27 nops, apenas 23 deve ser executadas até a primeira execução de j FIM
